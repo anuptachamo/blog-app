e324a57e86ea46ed70ea0c1ad0fda7e1b5c41af3
Anup Tachamo <anuptachamo@gmail.com>
2023-08-21 22:14:49 +0545

    Today all about express in nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express') //require express module
+const app = express()      //calls express function to start new Express app
+app.listen(3000 , () =>{
+    console.log("App listening on port 3000")
+})
+
+
+app.get('/', (req, res ) => {
+    res.json({
+        name: 'Anup Tachamo'
+    })
+})
